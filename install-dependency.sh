7118f9ef23d4fb83823ad5189e3bdd9d087a71ad
Meng Xu <mengxu@cis.upenn.edu>
2016-07-29 10:58:37 -0400

    install dependency

---
+++ install-dependency.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install libglib2.0-dev cmake openjdk-6-jdk python-dev python-gtk2 libgtk2.0-dev mesa-common-dev libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev libjpeg-dev -y
